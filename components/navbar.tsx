a0902b8154851d2be0e64ed847165682ff016501
Landry B <landry@quickdo.fr>
2024-05-16 13:17:38 +0100

    add responsive mobile navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,11 +2,14 @@
 
 import { cn } from '@/lib/utils'
 import { UserButton } from '@clerk/nextjs'
+import { Menu } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import Logo from './logo'
+import { useState } from 'react'
+import Logo, { LogoMobile } from './logo'
 import { ThemeSwitcherBtn } from './theme-swuitcher-btn'
-import { buttonVariants } from './ui/button'
+import { Button, buttonVariants } from './ui/button'
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet'
 
 const items = [
   { label: 'Accueil', link: '/' },
@@ -19,13 +22,14 @@ const Navbar = () => {
   return (
     <>
       <DesktopNavbar />
+      <MobileNavbar />
     </>
   )
 }
 
 const DesktopNavbar = () => {
   return (
-    <div className='hidden md:block border-b bg-background'>
+    <div className='hidden md:block border-separate border-b bg-background'>
       <nav className="container flex items-center justify-between px-8">
         <div className='flex h-[80px] min-h-[60px] items-center gap-x-4'>
           <Logo />
@@ -48,9 +52,49 @@ const DesktopNavbar = () => {
   )
 }
 
-const NavbarItem = ({ link, label }: { link: string, label: string }) => {
+const MobileNavbar = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <div className="block border-separate bg-backgound md:hidden">
+      <nav className="container flex items-center justify-between px-8">
+        <Sheet open={isOpen} onOpenChange={setIsOpen}>
+          <SheetTrigger>
+            <Button variant='ghost' size='icon'>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="w-[400px] sm:w-[540px]" side='left'>
+            <Logo />
+            <div className='flex flex-col gap-2 pt-4'>
+              {items.map((item) => (
+                <NavbarItem
+                  key={item.label}
+                  label={item.label}
+                  link={item.link}
+                  clickCallback={() => setIsOpen((open) => !open)}
+                />
+              ))}
+            </div>
+          </SheetContent>
+        </Sheet>
+        <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+          <LogoMobile />
+        </div>
+        <div className="flex items-center gap-2">
+          <ThemeSwitcherBtn />
+          <UserButton afterSignOutUrl='/' />
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+const NavbarItem = ({ link, label, clickCallback }: { link: string, label: string, clickCallback?: () => void }) => {
   const pathName = usePathname()
   const isActive = pathName === link
+
+
 
   return (
     <div className="relative flex items-center">
@@ -60,6 +104,9 @@ const NavbarItem = ({ link, label }: { link: string, label: string }) => {
         }),
         "w-full justify-start text-lg text-muted-foreground hover:text-foreground",
         isActive && "text-foreground")}
+        onClick={() => {
+          if (clickCallback) clickCallback();
+        }}
       >
         {label}
       </Link>
