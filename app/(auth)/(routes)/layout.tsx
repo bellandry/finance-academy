c7fc300e39d449b0893945ef6bf70b9e6c6c06b8
bellandry <bellandry.work@gmail.com>
2024-05-16 08:13:43 +0100

    add logo and edit login/signin/template

--- app/(auth)/(routes)/layout.tsx
+++ app/(auth)/(routes)/layout.tsx
@@ -1,3 +1,4 @@
+import Logo from "@/components/logo";
 import Image from "next/image";
 
 const AuthLayout = (
@@ -8,16 +9,20 @@ const AuthLayout = (
   }
 ) => {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <div className="relative w-full h-full md:w-1/2 hidden md:block">
-      <Image
-            fill
-            className="object-cover"
-            alt="login & signin image"
-            src="https://images.unsplash.com/photo-1541178735493-479c1a27ed24?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
+    <div className="h-screen flex items-center justify-center py-4">
+      <div className="w-full h-screen md:w-1/2 md:block fixed top-0 bottom-0 left-0">
+        <Image
+          layout="fill" objectFit="cover"
+          alt="login & signin image"
+          src="https://images.unsplash.com/photo-1633158829556-6ea20ad39b4f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
       </div>
-      <div className="w-full md:w-1/2 flex items-center justify-center">
+      <div className=" md:block md:w-1/2">
+        <div className="absolute m-4 z-10 top-4 left-3">
+          <Logo />
+        </div>
+      </div>
+      <div className="w-full md:w-1/2 flex items-center justify-center flex-col">
         {children}
       </div>
     </div>
---
+++ components/logo.tsx
@@ -0,0 +1,13 @@
+import { PiggyBank } from 'lucide-react'
+import React from 'react'
+
+const Logo = () => {
+  return (
+    <a href='/' className='flex items-center gap-2 z-10'>
+      <PiggyBank className='stroke h-10 w-10 stroke-amber-600 stroke-[1.5]' />
+      <p className='bg-gradient-to-r from-amber-400 to-orange-600 bg-clip-text text-2xl font-bold leading-light tracking-tighter text-transparent'>Finance Academy</p>
+    </a>
+  )
+}
+
+export default Logo
