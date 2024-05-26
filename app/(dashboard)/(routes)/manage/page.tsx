2044a80563b6a66fd17a131fb7f55403584af61b
bellandry <bellandry.work@gmail.com>
2024-05-26 21:54:36 +0100

    add manage and transaction page

---
+++ app/(dashboard)/(routes)/manage/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import React from 'react'
+
+const Manage = () => {
+  return (
+    <div className='border-b bg-card'>
+      <div className="contaiser">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Manage
